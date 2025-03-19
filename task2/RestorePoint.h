0a8af781135f5baa23b4e54074f260128ab8697b
52AnTIMateria812 <sysoevtim19@gmail.com>
2025-03-19 10:55:21 +0300

    Update RestorePoint.h

--- task2/RestorePoint.h
+++ task2/RestorePoint.h
@@ -1,19 +1,15 @@
 #pragma once
 #include <vector>
 #include <string>
-#include <chrono>
+#include <ctime>
 
 class RestorePoint {
+public:
+    RestorePoint(const std::vector<std::string>& files, std::time_t timestamp);
+    std::time_t getTimestamp() const;
+    const std::vector<std::string>& getStoredFiles() const;
+
 private:
     std::time_t timestamp;
     std::vector<std::string> storedFiles;
-    
-public:
-    RestorePoint(const std::vector<std::string>& files)
-        : timestamp(std::chrono::system_clock::to_time_t(
-              std::chrono::system_clock::now())),
-          storedFiles(files) {}
-
-    std::time_t getTimestamp() const { return timestamp; }
-    const std::vector<std::string>& getStoredFiles() const { return storedFiles; }
 };
